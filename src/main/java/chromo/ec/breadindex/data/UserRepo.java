4037b54ec4ff8d2572c921f72ffadd8c9e7b7db2
chromohh <sindre.he@hotmail.com>
2020-04-01 12:11:11 +0200

    Repositories added

--- src/main/java/chromo/ec/breadindex/data/UserRepo.java
+++ src/main/java/chromo/ec/breadindex/data/UserRepo.java
@@ -1,9 +1,20 @@
 package chromo.ec.breadindex.data;
 
+import chromo.ec.breadindex.entity.Bread;
 import chromo.ec.breadindex.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepo extends CrudRepository<User, Integer> {
+
+    @Query("SELECT u FROM User u WHERE u.userName LIKE %:userName%")
+    List<Bread> findByUserName(@Param("userName") String Name);
+
+    @Query("SELECT u FROM User u WHERE u.email LIKE %:email%")
+    List<Bread> findByEmail(@Param("email") String email);
 }
