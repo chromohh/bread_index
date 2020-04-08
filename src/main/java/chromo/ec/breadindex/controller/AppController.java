8a140f06ce8b74c00b533e58c2ed9d9c4f830d80
chromohh <sindre.he@hotmail.com>
2020-04-08 23:48:51 +0200

    Service created and minor edits

--- src/main/java/chromo/ec/breadindex/controller/AppController.java
+++ src/main/java/chromo/ec/breadindex/controller/AppController.java
@@ -2,12 +2,11 @@ package chromo.ec.breadindex.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
 
-    @GetMapping("/index")
+    @GetMapping("/public")
     public String index(){
         return "index";
     }
--- src/main/java/chromo/ec/breadindex/security/ApplicationSecurityConfig.java
+++ src/main/java/chromo/ec/breadindex/security/ApplicationSecurityConfig.java
@@ -1,4 +1,30 @@
 package chromo.ec.breadindex.security;
 
-public class ApplicationSecurityConfig {
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeRequests()
+                  .antMatchers("/admin/**").hasAuthority("ADMIN")
+                  .antMatchers("/users/**").hasAuthority("USER")
+                  .antMatchers("/public/**").permitAll()
+                .and()
+                  .formLogin()
+                  .loginPage("/public/login")
+                  .loginProcessingUrl("/public/login")
+                  .usernameParameter("email")
+                  .passwordParameter("password")
+                  .permitAll()
+                .and()
+                  .logout()
+                  .logoutUrl("/logout")
+                  .invalidateHttpSession(true)
+                  .deleteCookies("JSESSIONID")
+                  .logoutSuccessUrl("/login?logout")
+                .and()
+                  .exceptionHandling()
+                  .accessDeniedPage("/accessDenied");
+    }
 }
